8fa0f95834e1ed42b3d5325978860391f2ebaa71
root <aminoxlhd>
2024-01-04 11:30:21 -0800

    add 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - a function that inserts a node as the right-child of another node
+ * binary_tree_insert_right - a function that inserts a node as the right-child
  * @parent: is a pointer to the node to insert the right-child in
  * @value: is the value to store in the new node
- * Return: a pointer to the created node, or NULL on failure or if parent is NULL
+ * Return: a pointer to the created node, or NULL on failure or if parent NULL
  **/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
